d2f6845600f1c4983d525a0643d12b22ccfb3acd
github-user-64 <wrzh17350237062@outlook.com>
2026-01-25 11:09:01 +0800

    添加批量生成

---
+++ Mods/批量生成/批量生成/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace BatchSapwn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "生成配置.txt");
+
+                SpawnConfig config = SpawnConfig.TryLoad(path);
+                if (config == null) throw new Exception("配置为null");
+                config.Check();
+
+                Console.WriteLine($"从:{config.form}");
+                Console.WriteLine($"到:{config.to}");
+                for (int i = 0; i < config.mods.Count; ++i)
+                {
+                    Console.WriteLine($"{i}:{config.mods[i]}");
+                }
+
+                Console.WriteLine("准备开始生成");
+                Console.ReadLine();
+                Console.Clear();
+                Spawn(config);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("ok");
+            Console.ReadLine();
+        }
+
+        private static void Spawn(SpawnConfig config)
+        {
+            config.For((form, to, name) =>
+            {
+                form = Path.Combine(form, name);
+                to = Path.Combine(to, name);
+
+                Directory.CreateDirectory(to);
+
+                CopyFile(form, to, "info.json");
+                CopyFile(form, to, "loadConfig.json");
+                CopyFile(form, to, "ico.png");
+                CopyFile(form, to, $"{name}.dll");
+            });
+        }
+
+        private static void CopyFile(string form, string to, string file)
+        {
+            form = Path.Combine(form, file);
+            to = Path.Combine(to, file);
+
+            if (File.Exists(form) == false)
+            {
+                Console.WriteLine($"文件不存在, 跳过[{form}]");
+                return;
+            }
+            File.Copy(form, to);
+        }
+    }
+}
---
+++ Mods/批量生成/批量生成/SpawnConfig.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace BatchSapwn
+{
+    internal class SpawnConfig
+    {
+        public delegate void ForMod(string form, string to, string name);
+
+        public string form = "C:\\";
+        public string to = "C:\\";
+        public List<string> mods = new List<string>();
+
+        public void Check()
+        {
+            if (form == null) throw new Exception($"{nameof(form)}为null");
+            if (to == null) throw new Exception($"{nameof(to)}为null");
+            if (mods == null) throw new Exception($"{nameof(mods)}为null");
+        }
+
+        public void For(ForMod action)
+        {
+            if (action == null) return;
+            if (mods == null) return;
+
+            mods.ForEach(mod => action(form, to, mod));
+        }
+
+        public static SpawnConfig TryLoad(string FilePath1)
+        {
+            try
+            {
+                return Get2(FilePath1);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                Save(new SpawnConfig(), FilePath1, true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return null;
+        }
+
+        public static SpawnConfig Get2(string FilePath1)
+        {
+            try
+            {
+                if (!File.Exists(FilePath1)) throw new Exception($"文件不存在:{FilePath1}");
+
+                return JsonConvert.DeserializeObject<SpawnConfig>(File.ReadAllText(FilePath1, Encoding.UTF8));
+            }
+            catch (Exception ex)
+            {
+                MethodBase mb = MethodBase.GetCurrentMethod();
+                throw new Exception($"{mb?.DeclaringType.Name}.{mb?.Name}:{ex.Message}");
+            }
+        }
+
+        public static void Save(object val, string FilePath1, bool indented = false)
+        {
+            try
+            {
+                string directory = Path.GetDirectoryName(FilePath1);
+
+                if (!Directory.Exists(directory)) throw new Exception($"目录不存在:{directory}");
+
+                string text = null;
+                if (indented) text = JsonConvert.SerializeObject(val, Formatting.Indented);
+                else text = JsonConvert.SerializeObject(val);
+
+                File.WriteAllText(FilePath1, text, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                MethodBase mb = MethodBase.GetCurrentMethod();
+                throw new Exception($"{mb?.DeclaringType.Name}.{mb?.Name}:{ex.Message}");
+            }
+        }
+    }
+}
