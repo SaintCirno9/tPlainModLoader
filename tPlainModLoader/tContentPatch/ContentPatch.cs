4ecbc9b4acb9e55fd938dc68aa899a5d4be57801
github-user-64 <wrzh17350237062@outlook.com>
2026-02-28 22:58:25 +0800

    忘了改发布的版本

--- tPlainModLoader/tContentPatch/ContentPatch.cs
+++ tPlainModLoader/tContentPatch/ContentPatch.cs
@@ -16,7 +16,7 @@ namespace tContentPatch
         /// <summary/>
         public static bool Initialized { get; private set; } = false;
         /// <summary/>
-        public static bool NoPublic = false;
+        public static bool NoPublic = true;
 
         internal const string patchId_tContentPatch = "tContentPatch.gamePatch";
         internal const string patchId_mod = "tContentPatch.mod.patch";
