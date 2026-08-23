56215ed3f6b6773bef6ffb69a55a7b0a44a3845d
SaintCirno9 <lorisgilgamesh@126.com>
2026-08-23 17:07:48 +0800

    SDK 风格 net472 现代化：转换 5 个核心工程 csproj，并入 CommandHelp 源码（main 分支版本，替换原 gitlink），NuGet 转 PackageReference，XNA 走 GAC 硬路径，Terraria.exe 走 TerrariaDir 属性

---
+++ tPlainModLoader/CommandHelp/CommandMethod.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandHelp
+{
+    public class CommandMethod : CommandObject
+    {
+        private readonly int _argCount = 0;
+        public int ArgCount => _argCount;
+        public event Action<object[]> Runing = null;
+
+
+        public CommandMethod(string text = null, int argCount = 0) : base(text)
+        {
+            if (argCount < 0) throw new ArgumentException("参数不能小于0", nameof(argCount));
+            _argCount = argCount;
+        }
+
+
+        public override object Run(ref int index, List<CommandObject> commandList)
+        {
+            if (HasPrintList(ref index, commandList)) return null;
+
+            //
+
+            if (commandList == null) throw new ArgumentNullException(nameof(commandList));
+            int rightCount = commandList.Count - index - 1;
+            if (_argCount > rightCount) throw new Exceptions.CommandException(exceptionmessage: $"缺少{_argCount - rightCount}个参数");
+
+            object[] args = new object[_argCount];
+
+            for (int i = 0; i < _argCount; ++i)
+            {
+                ++index;
+
+                object arg = commandList[index].Run(ref index, commandList);
+                args[i] = arg;
+            }
+
+            return OnRuning(ref index, commandList, args);
+        }
+
+        public virtual object OnRuning(ref int index, List<CommandObject> commandList, object[] args)
+        {
+            Runing?.Invoke(args);
+
+            return null;
+        }
+
+        public virtual bool HasPrintList(ref int index, List<CommandObject> commandList)
+        {
+            for (int i = 0;
+                i < ArgCount &&
+                i + index + 1 < commandList.Count;
+                ++i)
+            {
+                if (commandList[index + i + 1] is CommandPrintList cpl == false) continue;
+
+                index += i + 1;
+                cpl.Run(ref index, commandList);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tPlainModLoader/CommandHelp/CommandPrintList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandHelp
+{
+    /// <summary>
+    /// 显示指令列表
+    /// </summary>
+    public class CommandPrintList : CommandMethod
+    {
+        public override string Text => "?";
+        public List<CommandObject> Cos { get; protected set; } = null;
+        public Action<string> Print { get; protected set; } = null;
+        public string Tip { get; protected set; } = null;
+
+        /// <summary>
+        /// 显示指令列表
+        /// </summary>
+        public CommandPrintList(List<CommandObject> cos, string tip = null, Action<string> print = null)
+        {
+            Cos = cos;
+            Tip = tip;
+            Print = print;
+        }
+
+        public override object OnRuning(ref int index, List<CommandObject> commandList, object[] args)
+        {
+            base.OnRuning(ref index, commandList, args);
+
+            string s = null;
+
+            Cos?.ForEach(i =>
+            {
+                if (i == this) return;
+                if (i == null) return;
+
+                if (i is CommandeEnum enu)
+                {
+                    foreach (string enui in enu.Enums)
+                    {
+                        if (s == null)
+                        {
+                            s = $"{enui}";
+                        }
+                        else
+                        {
+                            s += $", {enui}";
+                        }
+                    }
+
+                    return;
+                }
+
+                if (s == null)
+                {
+                    s = $"{i.Text}";
+                }
+                else
+                {
+                    s += $", {i.Text}";
+                }
+            });
+
+            if (s == null) s = "no cmd";
+            if (Tip != null) s = $"{s}//{Tip}";
+            Print?.Invoke(s);
+
+            return this;
+        }
+    }
+}
