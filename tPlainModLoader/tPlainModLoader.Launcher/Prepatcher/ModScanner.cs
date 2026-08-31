adcb2ea79fe60fa85c548f912e61a9320e3af1aa
SaintCirno9 <lorisgilgamesh@126.com>
2026-09-01 04:15:19 +0800

    fix(core): 核心程序集更名为 TPML.dll 彻底解决同名程序集混淆 TypeLoadException

--- tPlainModLoader/tPlainModLoader.Launcher/Prepatcher/ModScanner.cs
+++ tPlainModLoader/tPlainModLoader.Launcher/Prepatcher/ModScanner.cs
@@ -72,7 +72,8 @@ namespace tPlainModLoader.Prepatcher
                         foreach (string dllFile in Directory.GetFiles(modSubDir, "*.dll"))
                         {
                             string fileName = Path.GetFileName(dllFile);
-                            if (fileName.Equals("tPlainModLoader.dll", StringComparison.OrdinalIgnoreCase) ||
+                            if (fileName.Equals("TPML.dll", StringComparison.OrdinalIgnoreCase) ||
+                                fileName.Equals("tPlainModLoader.dll", StringComparison.OrdinalIgnoreCase) ||
                                 fileName.Equals("tPlainModLoader.exe", StringComparison.OrdinalIgnoreCase) ||
                                 fileName.Equals("TerrariaHooks.dll", StringComparison.OrdinalIgnoreCase) ||
                                 fileName.Equals("TPML.TerrariaHooks.dll", StringComparison.OrdinalIgnoreCase) ||
